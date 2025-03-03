2cc96cf5b3fcf1e08e46d1448e38afce9ed5603b
bramlaurens <bramlaurens@gmail.com>
2025-03-03 20:57:18 +0100

    init2

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
+#include <HardwareSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define RX_GPIO 26
+#define TX_GPIO 27
+
+int counter = 0;
+
+HardwareSerial Serial3(2);
 
 void setup() {
+  Serial.begin(115200);
+  Serial3.begin(115200, SERIAL_8N1, RX_GPIO, TX_GPIO);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (Serial3.available()>0) 
+  {
+    String RXdata = Serial3.readStringUntil('\n');
+    Serial.print("Received: " + RXdata);
+  }
+  
 }

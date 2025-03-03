c6cbbbf841d1296f35d631802b2633a284e110a3
bramlaurens <bramlaurens@gmail.com>
2025-03-03 19:06:56 +0100

    Init2

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
+#include <HardwareSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define RX_GPIO 26
+#define TX_GPIO 27
+
+int counter = 0;
+
+HardwareSerial Serial3(2);
 
 void setup() {
+  Serial.begin(115200);
+  Serial3.begin(115200, SERIAL_8N1, RX_GPIO, TX_GPIO);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial3.println(String(counter));
+
+  counter++;
+  delay(1000);
 }

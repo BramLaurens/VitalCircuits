c58ae2e124680883e88568b60f8f59ae26d71bf0
Fabian Meijneken <fmeijneken@gmail.com>
2025-03-04 15:18:08 +0100

    Finished `main.cpp` and did some cleanup. The final message should now have the correct format.

--- src/main.cpp
+++ src/main.cpp
@@ -4,88 +4,79 @@
 
 RH_ASK driver;
 
-struct sensorData {
+
+struct sensordata_struct
+{
     unsigned char pressure_sensor;        // 0 - 255
-    int temperature_sensor;                // -32,768 - 32,767
-    short unsigned int moisture_sensor;       // 0 - 65,535
-    unsigned char heartbeat[10];             // 0 - 255
+    int temperature_sensor;               // -32,768 - 32,767
+    short unsigned int moisture_sensor;   // 0 - 65,535
+    unsigned char heartbeat[10];          // 0 - 255
 };
 
-
-struct protocol{
+struct message_struct
+{
   char soc;
   char pl;
   char src_ID;
-  char des_ID
+  char des_ID;
   char pc;
   char functiecode;
-  sensorData data;
+  sensordata_struct data;
   char eot;
   char lrc;   
-}
+};
+
+sensordata_struct test_data;
+message_struct message;
 
 
+void setup() 
+{
+  Serial.begin(9600);
 
+  if (!driver.init()) 
+  {
+    Serial.println("init failed");
+  }
 
-
-void setup() {
-    Serial.begin(9600);
-    if (!driver.init()) {
-        Serial.println("init failed");
-    }
-      // Create a test struct with values from 0 - 100
-  testData = {
-    11,  // pressure_sensor
-    50,                      // temperature_sensor
-    100,                                      // moisture_sensor
-    {0}                                       // heartbear (initialize all to 0)
+  // Create test data
+  // Evantually this will be replaced by real sensor data.
+  test_data = {
+    11,       // pressure_sensor
+    50,       // temperature_sensor
+    100,      // moisture_sensor
+    {0}       // heartbear (initialize all to 0)
   };
-    // Optionally, fill heartbear with values from 0 to 99
+
+  // Fill heartbear with values from 0 to 99
   for (int i = 0; i < 10; i++) {
-    testData.heartbeat[i]=i;
+    test_data.heartbeat[i]=i;
   }
 
-protocol eind_structuur
-eind_structuur.soc = 1;
-eind_structuur.pl = sizeof(eind_structuur);
-eind_structuur.src_ID = 0000;
-eind_structuur.des_ID = 0100;
-eind_structuur.pc= 3;
-eind_structuur.functiecode= 2;
-eind_structuur.data= testData;
-eind_structuur.eot =2;
-eind_structuur.lrc = 00;
+  // Create the final message.
+  message.soc = 1;                                    // Start of communication
+  message.pl = sizeof(message) - sizeof(message.lrc); // The packet length is the size of the total message minus the lrc part.
+  message.src_ID = 0x04;                              // (0x04 = 0000 0100) The source ID for EV1A Group 4.
+  message.des_ID = 0x04;                              // The destination ID, in this case EV1A Group 4.
+  message.pc= 1;                                      // The packet counter, in this case 1.
+  message.functiecode = 2;                            // The function code, in this case 2 (Data verzenden).
+  message.data = test_data;                           // The data
+  message.eot = 0xFF;                                 // End of transmission bit. This is FF in our case.
+  message.lrc = 0;                                    // TODO: The Longitudinal Redundancy Check
 
-
-
-  /*
-  protocool ={
-    soc = 1,
-    sizeoff,
-    00000100,
-    00000100,
-    pakket nummer,
-    functie code,
-    data,
-    2,
-    foutcontrole,
-  }
-  */
-
-  }
 }
-
 
 
 void loop() {
- 
-    //int number[10] = {1, 252, 3, 4, 5, 6, 7, 8, 9, 10}; // Getal dat je wilt verzenden
+    unsigned long start = millis(); // Save the current time (miliseconds since program start)
 
-    driver.send((uint8_t *)&testData, sizeof(testData)); // Verstuur als bytes
+    driver.send((uint8_t *)&message, sizeof(message)); 
     driver.waitPacketSent();
 
-    Serial.print("Struct verzonden!");
-    
 
+    // Data send and output the duration of the package send.
+    Serial.print("Data send. duration: ");
+    Serial.println(millis() - start);
+    
     delay(10);
 }

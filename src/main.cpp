11d1afd4fd02093f39c7f943a805a98ba1ca4c00
bramlaurens <bramlaurens@gmail.com>
2025-03-07 11:28:32 +0100

    Changed pins & baud

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
 
-#define RX_GPIO 26
-#define TX_GPIO 27
+#define RX_GPIO 16
+#define TX_GPIO 17
 
 int counter = 0;
 
 HardwareSerial Serial3(2);
 
 void setup() {
-  Serial.begin(115200);
-  Serial3.begin(115200, SERIAL_8N1, RX_GPIO, TX_GPIO);
+  Serial.begin(9600);
+  Serial3.begin(9600, SERIAL_8N1, RX_GPIO, TX_GPIO);
 
 }
 
 void loop() {
   while (Serial3.available()>0) 
   {
-    String RXdata = Serial3.readStringUntil('\n');
+    String RXdata = Serial3.readString();
     Serial.print("Received: " + RXdata);
   }
   

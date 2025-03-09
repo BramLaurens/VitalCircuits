c0bf31695f9ff712f31a00715d073cfef64e6b01
bramlaurens <bramlaurens@gmail.com>
2025-03-09 22:53:36 +0100

    Changed config method

--- src/main.cpp
+++ src/main.cpp
@@ -1,131 +1,190 @@
-#define FREQUENCY_868
+/*
+ * LoRa E220
+ * Set configuration.
+ *
+ * You must uncommend the correct constructor.
+ *
+ * by Renzo Mischianti <https://www.mischianti.org>
+ *
+ * https://www.mischianti.org
+ *
+ * E220		  ----- WeMos D1 mini	----- esp32			----- Arduino Nano 33 IoT	----- Arduino MKR	----- Raspberry Pi Pico   ----- stm32               ----- ArduinoUNO
+ * M0         ----- D7 (or 3.3v)	----- 19 (or 3.3v)	----- 4 (or 3.3v)			----- 2 (or 3.3v)	----- 10 (or 3.3v)	      ----- PB0 (or 3.3v)       ----- 7 Volt div (or 3.3v)
+ * M1         ----- D6 (or 3.3v)	----- 21 (or 3.3v)	----- 6 (or 3.3v)			----- 4 (or 3.3v)	----- 11 (or 3.3v)	      ----- PB10 (or 3.3v)      ----- 6 Volt div (or 3.3v)
+ * TX         ----- D3 (PullUP)		----- TX2 (PullUP)	----- TX1 (PullUP)			----- 14 (PullUP)	----- 8 (PullUP)	      ----- PA2 TX2 (PullUP)    ----- 4 (PullUP)
+ * RX         ----- D4 (PullUP)		----- RX2 (PullUP)	----- RX1 (PullUP)			----- 13 (PullUP)	----- 9 (PullUP)	      ----- PA3 RX2 (PullUP)    ----- 5 Volt div (PullUP)
+ * AUX        ----- D5 (PullUP)		----- 18  (PullUP)	----- 2  (PullUP)			----- 0  (PullUP)	----- 2  (PullUP)	      ----- PA0  (PullUP)       ----- 3 (PullUP)
+ * VCC        ----- 3.3v/5v			----- 3.3v/5v		----- 3.3v/5v				----- 3.3v/5v		----- 3.3v/5v		      ----- 3.3v/5v             ----- 3.3v/5v
+ * GND        ----- GND				----- GND			----- GND					----- GND			----- GND			      ----- GND                 ----- GND
+ *
+ */
 #define LoRa_E220_DEBUG
+#define FREQUENCY_868
 
-#include <Arduino.h>
-#include <RH_ASK.h>
-#include <SPI.h> // Not actually used but needed to compile
-#include <LoRa_E220.h>
+#include "Arduino.h"
+#include "LoRa_E220.h"
 
-#define RX_GPIO 16
-#define TX_GPIO 17
 
-LoRa_E220 LoraTTL(&Serial2, 15, 21, 19);
+// ---------- esp8266 pins --------------
+//LoRa_E220 e220ttl(RX, TX, AUX, M0, M1);  // Arduino RX <-- e220 TX, Arduino TX --> e220 RX
+//LoRa_E220 e220ttl(D3, D4, D5, D7, D6); // Arduino RX <-- e220 TX, Arduino TX --> e220 RX AUX M0 M1
+//LoRa_E220 e220ttl(D2, D3); // Config without connect AUX and M0 M1
 
-void printLoRaParameters(struct Configuration configuration);
-void printLoRaModuleInformation(struct ModuleInformation moduleInformation);
-void LoRaSetConfig();
+//#include <SoftwareSerial.h>
+//SoftwareSerial mySerial(D2, D3); // Arduino RX <-- e220 TX, Arduino TX --> e220 RX
+//LoRa_E220 e220ttl(&mySerial, D5, D7, D6); // AUX M0 M1
+// -------------------------------------
 
-struct testData {
-  int testint;
-  float testfloat;
-  bool testbool;
-};
+// ---------- Arduino pins --------------
+//LoRa_E220 e220ttl(4, 5, 3, 7, 6); // Arduino RX <-- e220 TX, Arduino TX --> e220 RX AUX M0 M1
+//LoRa_E220 e220ttl(4, 5); // Config without connect AUX and M0 M1
 
-testData testdata = {1, 2.3, true};
+//#include <SoftwareSerial.h>
+//SoftwareSerial mySerial(4, 5); // Arduino RX <-- e220 TX, Arduino TX --> e220 RX
+//LoRa_E220 e220ttl(&mySerial, 3, 7, 6); // AUX M0 M1
+// -------------------------------------
 
-void setup()
-{
-  //Serial setup
-  Serial.begin(56700);
-  Serial2.begin(56700, SERIAL_8N1, 16, 17);
-  while(!Serial){};
-  delay(500);
-  Serial.println();
+// ------------- Arduino Nano 33 IoT -------------
+// LoRa_E220 e220ttl(&Serial1, 2, 4, 6); //  RX AUX M0 M1
+// -------------------------------------------------
 
-  if (LoraTTL.begin()) {
-    Serial.println("LoRa module initialized successfully");
-  } else {
-    Serial.println("Failed to initialize LoRa module");
+// ------------- Arduino MKR WiFi 1010 -------------
+// LoRa_E220 e220ttl(&Serial1, 0, 2, 4); //  RX AUX M0 M1
+// -------------------------------------------------
+
+// ---------- esp32 pins --------------
+LoRa_E220 e220ttl(&Serial2, 15, 21, 19); //  RX AUX M0 M1
+
+//LoRa_E220 e220ttl(&Serial2, 22, 4, 18, 21, 19, UART_BPS_RATE_9600); //  esp32 RX <-- e220 TX, esp32 TX --> e220 RX AUX M0 M1
+// -------------------------------------
+
+// ---------- Raspberry PI Pico pins --------------
+// LoRa_E220 e220ttl(&Serial2, 2, 10, 11); //  RX AUX M0 M1
+// -------------------------------------
+
+// ---------------- STM32 --------------------
+//HardwareSerial Serial2(USART2);   // PA3  (RX)  PA2  (TX)
+//LoRa_E220 e220ttl(&Serial2, PA0, PB0, PB10); //  RX AUX M0 M1
+// -------------------------------------------------
+
+void printParameters(struct Configuration configuration);
+void printModuleInformation(struct ModuleInformation moduleInformation);
+void SetLoRaConfig();
+
+void setup() {
+	Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
+	while(!Serial){};
+	delay(500);
+
+	Serial.println();
+
+
+	// Startup all pins and UART
+	e220ttl.begin();
+
+  //Set LoRa module config
+  SetLoRaConfig();
+}
+
+void loop() {
+    // If something available
+  if (e220ttl.available()>1) {
+      // read the String message
+    ResponseContainer rc = e220ttl.receiveMessage();
+    // Is something goes wrong print error
+    if (rc.status.code!=1){
+        Serial.println(rc.status.getResponseDescription());
+    }else{
+        // Print the data received
+        Serial.println(rc.data);
+    }
   }
-
-  LoRaSetConfig();
-}
-
-void loop()
-{
-  if (LoraTTL.available()>1) {
-    // read the String message
-  ResponseContainer rc = LoraTTL.receiveMessage();
-  // Is something goes wrong print error
-  if (rc.status.code!=1){
-      Serial.println(rc.status.getResponseDescription());
-  }else{
-      // Print the data received
-      Serial.println(rc.data);
+  if (Serial.available()) {
+      String input = Serial.readString();
+      Serial.println("Sent:" + input);
+      ResponseStatus rs = e220ttl.sendMessage(input);
+      Serial.println(rs.getResponseDescription());
   }
 }
+
+void SetLoRaConfig(){
+  ResponseStructContainer c;
+	c = e220ttl.getConfiguration();
+	// It's important get configuration pointer before all other operation
+	Configuration configuration = *(Configuration*) c.data;
+	Serial.println(c.status.getResponseDescription());
+	Serial.println(c.status.code);
+
+	printParameters(configuration);
+
+
+	configuration.ADDL = 0x03;  // First part of address
+	configuration.ADDH = 0x00; // Second part
+
+	configuration.CHAN = 18; // Communication channel
+
+	configuration.SPED.uartBaudRate = UART_BPS_9600; // Serial baud rate
+	configuration.SPED.airDataRate = AIR_DATA_RATE_010_24; // Air baud rate
+	configuration.SPED.uartParity = MODE_00_8N1; // Parity bit
+
+	configuration.OPTION.subPacketSetting = SPS_200_00; // Packet size
+	configuration.OPTION.RSSIAmbientNoise = RSSI_AMBIENT_NOISE_DISABLED; // Need to send special command
+	configuration.OPTION.transmissionPower = POWER_22; // Device power
+
+	configuration.TRANSMISSION_MODE.enableRSSI = RSSI_DISABLED; // Enable RSSI info
+	configuration.TRANSMISSION_MODE.fixedTransmission = FT_TRANSPARENT_TRANSMISSION; // Enable repeater mode
+	configuration.TRANSMISSION_MODE.enableLBT = LBT_DISABLED; // Check interference
+	configuration.TRANSMISSION_MODE.WORPeriod = WOR_2000_011; // WOR timing
+
+	// Set configuration changed and set to not hold the configuration
+	ResponseStatus rs = e220ttl.setConfiguration(configuration, WRITE_CFG_PWR_DWN_SAVE);
+	Serial.println(rs.getResponseDescription());
+	Serial.println(rs.code);
+
+	c = e220ttl.getConfiguration();
+	// It's important get configuration pointer before all other operation
+	configuration = *(Configuration*) c.data;
+	Serial.println(c.status.getResponseDescription());
+	Serial.println(c.status.code);
+
+	printParameters(configuration);
+	c.close();
 }
 
-void LoRaSetConfig(){
-  ResponseStructContainer c; //Make a struct container for the response from event on the LoRa module (data, rssi, status)
-  c = LoraTTL.getConfiguration(); //Get the current configuration of the LoRa module and store the return in the struct container
-  Configuration LoraConfig = *(Configuration *) c.data; //Type cast to make sure the retreived data is actually stored in the config struct
-  Serial.println(c.status.getResponseDescription());
-  Serial.println(c.status.code);
+void printParameters(struct Configuration configuration) {
+	DEBUG_PRINTLN("----------------------------------------");
 
-  //printLoRaParameters(LoraConfig);
+	DEBUG_PRINT(F("HEAD : "));  DEBUG_PRINT(configuration.COMMAND, HEX);DEBUG_PRINT(" ");DEBUG_PRINT(configuration.STARTING_ADDRESS, HEX);DEBUG_PRINT(" ");DEBUG_PRINTLN(configuration.LENGHT, HEX);
+	DEBUG_PRINTLN(F(" "));
+	DEBUG_PRINT(F("AddH : "));  DEBUG_PRINTLN(configuration.ADDH, HEX);
+	DEBUG_PRINT(F("AddL : "));  DEBUG_PRINTLN(configuration.ADDL, HEX);
+	DEBUG_PRINTLN(F(" "));
+	DEBUG_PRINT(F("Chan : "));  DEBUG_PRINT(configuration.CHAN, DEC); DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.getChannelDescription());
+	DEBUG_PRINTLN(F(" "));
+	DEBUG_PRINT(F("SpeedParityBit     : "));  DEBUG_PRINT(configuration.SPED.uartParity, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.SPED.getUARTParityDescription());
+	DEBUG_PRINT(F("SpeedUARTDatte     : "));  DEBUG_PRINT(configuration.SPED.uartBaudRate, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.SPED.getUARTBaudRateDescription());
+	DEBUG_PRINT(F("SpeedAirDataRate   : "));  DEBUG_PRINT(configuration.SPED.airDataRate, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.SPED.getAirDataRateDescription());
+	DEBUG_PRINTLN(F(" "));
+	DEBUG_PRINT(F("OptionSubPacketSett: "));  DEBUG_PRINT(configuration.OPTION.subPacketSetting, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.OPTION.getSubPacketSetting());
+	DEBUG_PRINT(F("OptionTranPower    : "));  DEBUG_PRINT(configuration.OPTION.transmissionPower, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.OPTION.getTransmissionPowerDescription());
+	DEBUG_PRINT(F("OptionRSSIAmbientNo: "));  DEBUG_PRINT(configuration.OPTION.RSSIAmbientNoise, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.OPTION.getRSSIAmbientNoiseEnable());
+	DEBUG_PRINTLN(F(" "));
+	DEBUG_PRINT(F("TransModeWORPeriod : "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.WORPeriod, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getWORPeriodByParamsDescription());
+	DEBUG_PRINT(F("TransModeEnableLBT : "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.enableLBT, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getLBTEnableByteDescription());
+	DEBUG_PRINT(F("TransModeEnableRSSI: "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.enableRSSI, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getRSSIEnableByteDescription());
+	DEBUG_PRINT(F("TransModeFixedTrans: "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.fixedTransmission, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getFixedTransmissionDescription());
 
-  
-  LoraConfig.ADDL = 0x02;  // Low byte of address
-  LoraConfig.ADDH = 0x00; // High byte of address
-  LoraConfig.CHAN = 18;   // Channel
 
-  LoraConfig.SPED.uartBaudRate = UART_BPS_57600;
-  LoraConfig.SPED.uartParity = MODE_00_8N1;
-  LoraConfig.SPED.airDataRate = AIR_DATA_RATE_111_625;
-
-  LoraConfig.OPTION.subPacketSetting = SPS_200_00;
-  LoraConfig.OPTION.RSSIAmbientNoise = RSSI_AMBIENT_NOISE_DISABLED;
-  LoraConfig.OPTION.transmissionPower = POWER_22;
-
-  LoraConfig.TRANSMISSION_MODE.enableRSSI = RSSI_DISABLED;
-  LoraConfig.TRANSMISSION_MODE.fixedTransmission = FT_TRANSPARENT_TRANSMISSION;
-  LoraConfig.TRANSMISSION_MODE.enableLBT = LBT_DISABLED;
-  LoraConfig.TRANSMISSION_MODE.WORPeriod = WOR_2000_011;
-
-  LoraConfig.CRYPT.CRYPT_H = 0x00;
-  LoraConfig.CRYPT.CRYPT_L = 0x00;
-
-  ResponseStatus LoRaRS = LoraTTL.setConfiguration(LoraConfig, WRITE_CFG_PWR_DWN_SAVE);
-  Serial.println(c.status.getResponseDescription());
-  Serial.println(c.status.code);
-
-  printLoRaParameters(LoraConfig);
-  
-  
-  c.close(); //Close and clear the struct container
-
+	DEBUG_PRINTLN("----------------------------------------");
 }
+void printModuleInformation(struct ModuleInformation moduleInformation) {
+	Serial.println("----------------------------------------");
+	DEBUG_PRINT(F("HEAD: "));  DEBUG_PRINT(moduleInformation.COMMAND, HEX);DEBUG_PRINT(" ");DEBUG_PRINT(moduleInformation.STARTING_ADDRESS, HEX);DEBUG_PRINT(" ");DEBUG_PRINTLN(moduleInformation.LENGHT, DEC);
 
-void printLoRaParameters(struct Configuration configuration) {
-  DEBUG_PRINTLN("----------------------------------------");
-  DEBUG_PRINT(F("HEAD : "));  DEBUG_PRINT(configuration.COMMAND, HEX);DEBUG_PRINT(" ");DEBUG_PRINT(configuration.STARTING_ADDRESS, HEX);DEBUG_PRINT(" ");DEBUG_PRINTLN(configuration.LENGHT, HEX);
-  DEBUG_PRINTLN(F(" "));
-  DEBUG_PRINT(F("AddH : "));  DEBUG_PRINTLN(configuration.ADDH, HEX);
-  DEBUG_PRINT(F("AddL : "));  DEBUG_PRINTLN(configuration.ADDL, HEX);
-  DEBUG_PRINTLN(F(" "));
-  DEBUG_PRINT(F("Chan : "));  DEBUG_PRINT(configuration.CHAN, DEC); DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.getChannelDescription());
-  DEBUG_PRINTLN(F(" "));
-  DEBUG_PRINT(F("SpeedParityBit     : "));  DEBUG_PRINT(configuration.SPED.uartParity, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.SPED.getUARTParityDescription());
-  DEBUG_PRINT(F("SpeedUARTDatte     : "));  DEBUG_PRINT(configuration.SPED.uartBaudRate, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.SPED.getUARTBaudRateDescription());
-  DEBUG_PRINT(F("SpeedAirDataRate   : "));  DEBUG_PRINT(configuration.SPED.airDataRate, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.SPED.getAirDataRateDescription());
-  DEBUG_PRINTLN(F(" "));
-  DEBUG_PRINT(F("OptionSubPacketSett: "));  DEBUG_PRINT(configuration.OPTION.subPacketSetting, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.OPTION.getSubPacketSetting());
-  DEBUG_PRINT(F("OptionTranPower    : "));  DEBUG_PRINT(configuration.OPTION.transmissionPower, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.OPTION.getTransmissionPowerDescription());
-  DEBUG_PRINT(F("OptionRSSIAmbientNo: "));  DEBUG_PRINT(configuration.OPTION.RSSIAmbientNoise, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.OPTION.getRSSIAmbientNoiseEnable());
-  DEBUG_PRINTLN(F(" "));
-  DEBUG_PRINT(F("TransModeWORPeriod : "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.WORPeriod, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getWORPeriodByParamsDescription());
-  DEBUG_PRINT(F("TransModeEnableLBT : "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.enableLBT, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getLBTEnableByteDescription());
-  DEBUG_PRINT(F("TransModeEnableRSSI: "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.enableRSSI, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getRSSIEnableByteDescription());
-  DEBUG_PRINT(F("TransModeFixedTrans: "));  DEBUG_PRINT(configuration.TRANSMISSION_MODE.fixedTransmission, BIN);DEBUG_PRINT(" -> "); DEBUG_PRINTLN(configuration.TRANSMISSION_MODE.getFixedTransmissionDescription());
-  DEBUG_PRINTLN("----------------------------------------");
-}
+	Serial.print(F("Model no.: "));  Serial.println(moduleInformation.model, HEX);
+	Serial.print(F("Version  : "));  Serial.println(moduleInformation.version, HEX);
+	Serial.print(F("Features : "));  Serial.println(moduleInformation.features, HEX);
+	Serial.println("----------------------------------------");
 
-void printLoRaModuleInformation(struct ModuleInformation moduleInformation) {
-  Serial.println("----------------------------------------");
-  DEBUG_PRINT(F("HEAD: "));  DEBUG_PRINT(moduleInformation.COMMAND, HEX);DEBUG_PRINT(" ");DEBUG_PRINT(moduleInformation.STARTING_ADDRESS, HEX);DEBUG_PRINT(" ");DEBUG_PRINTLN(moduleInformation.LENGHT, DEC);
-  Serial.print(F("Model no.: "));  Serial.println(moduleInformation.model, HEX);
-  Serial.print(F("Version  : "));  Serial.println(moduleInformation.version, HEX);
-  Serial.print(F("Features : "));  Serial.println(moduleInformation.features, HEX);
-  Serial.println("----------------------------------------");
 }
